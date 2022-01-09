d25b00bbf88d03eddb075b75c55a2a356c007aa0
Dylan Box <dylanbox08@gmail.com>
2022-01-08 20:24:30 -0500

    New: Basic Setup for Looking Glass

---
+++ README.md
@@ -0,0 +1,11 @@
+# Looking Glass
+
+Smart mirror system built on top of [React NodeGUI](https://react.nodegui.org).
+
+## Development
+
+```
+npm install
+npm run dev
+npm run start
+```
---
+++ src/index.js
@@ -0,0 +1,21 @@
+/**
+ Life Cycle:
+  Setup
+  Register Plugins
+  Start UI
+  Main Loop
+      Check pluging
+
+**/
+import { Renderer } from "@nodegui/react-nodegui";
+import React from "react";
+import App from "./app";
+
+process.title = "Looking Glass";
+Renderer.render(<App />);
+// This is for hot reloading (this will be stripped off in production by webpack)
+if (module.hot) {
+  module.hot.accept(["./app"], function() {
+    Renderer.forceUpdate();
+  });
+}
